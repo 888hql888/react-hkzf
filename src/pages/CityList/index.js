2698edd4bc6dc51ce9ad536f2bd1dbbb15e5c59d
chienli <chienli@chienlideMacBook-Pro.local>
2021-10-17 23:24:03 +0800

    feat:新增CityList头部NavBar

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import { NavBar, Icon } from 'antd-mobile';
+import './index.scss'
+import axios from 'axios'
 
 export default class CityList extends Component {
+    state = {
+
+    }
+    componentDidMount() {
+        this.getCityList()
+      }
+    
+      // 获取城市列表数据的方法
+      async getCityList() {
+        const res = await axios.get('http://localhost:8080/area/city?level=1')
+        console.log('城市列表数据：', res)
+      }
+    
     render() {
         return (
-            <div>
-               <h1> CityList组件</h1>
+            <div className='cityList'>
+                <NavBar
+                    mode="light"
+                    icon={<Icon type="left" />}
+                    onLeftClick={() => this.props.history.go(-1)}
+                    className='navBar'
+                >城市列表</NavBar>
             </div>
         )
     }
