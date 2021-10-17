a897ec49c049219af4e5527dbdbf719d67910c29
chienli <chienli@chienlideMacBook-Pro.local>
2021-10-17 15:05:43 +0800

    feat:新增首页租房小组和搜索框UI结构

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react'
 
-import { Carousel } from 'antd-mobile';
-import { Flex } from 'antd-mobile';
+import { Carousel, WingBlank, Flex, Grid } from 'antd-mobile';
 import axios from 'axios'
 
 
@@ -41,11 +40,15 @@ const navs = [
         path: '/rent'
     }
 ]
+
+
 export default class Index extends Component {
     state = {
         data: ['1', '2', '3'],
         imgHeight: 220,
-        swiperArr: []
+        swiperArr: [],
+        groups: [],
+        news: []
     }
     async getSweipers() {
         const res = await axios.get('http://localhost:8080/home/swiper')
@@ -55,6 +58,50 @@ export default class Index extends Component {
                 swiperArr: data.body
             })
         }
+    }
+    // 获取租房小组数据的方法
+    async getGroups() {
+        const res = await axios.get('http://localhost:8080/home/groups', {
+            params: {
+                area: 'AREA%7C88cff55c-aaa4-e2e0'
+            }
+        })
+        // console.log(res)
+        this.setState({
+            groups: res.data.body
+        })
+    }
+    // 获取最新资讯
+    async getNews() {
+        const res = await axios.get(
+            'http://localhost:8080/home/news?area=AREA%7C88cff55c-aaa4-e2e0'
+        )
+
+        this.setState({
+            news: res.data.body
+        })
+    }
+
+    // 渲染最新资讯
+    renderNews() {
+        return this.state.news.map(item => (
+            <div className="news-item" key={item.id}>
+                <div className="imgwrap">
+                    <img
+                        className="img"
+                        src={`http://localhost:8080${item.imgSrc}`}
+                        alt=""
+                    />
+                </div>
+                <Flex className="content" direction="column" justify="between">
+                    <h3 className="title">{item.title}</h3>
+                    <Flex className="info" justify="between">
+                        <span>{item.from}</span>
+                        <span>{item.date}</span>
+                    </Flex>
+                </Flex>
+            </div>
+        ))
     }
     //渲染轮播图
     renderSwiper() {
@@ -76,8 +123,8 @@ export default class Index extends Component {
     //渲染首页导航 
     renderNavs() {
         //千万要记得  遍历返回标签要记得 return !!!!
-        return navs.map(item => 
-             <Flex.Item onClick={() => { this.props.history.push(item.path) }}>
+        return navs.map(item =>
+            <Flex.Item onClick={() => { this.props.history.push(item.path) }}>
                 <img
                     src={item.img}
                     alt=""
@@ -88,11 +135,15 @@ export default class Index extends Component {
     }
     componentDidMount() {
         this.getSweipers()
+        this.getGroups()
+        this.getNews()
+
     }
     render() {
         return (
             <div className='index'>
-                {/* 轮播图 */}
+
+                {/* 轮播图 start*/}
                 {/* Bug  轮播图必须等待有数据的时候才可以自动轮播 否则 卡住不动 */}
                 <div className='swipper'>
                     {
@@ -104,13 +155,78 @@ export default class Index extends Component {
                             {this.renderSwiper()}
                         </Carousel> : ''
                     }
-                </div>
-                {/* item 导航跳转区域 */}
+                    <Flex className="search-box">
+                        {/* 左侧白色区域 */}
+                        <Flex className="search">
+                            {/* 位置 */}
+                            <div
+                                className="location"
+                                onClick={() => this.props.history.push('/citylist')}
+                            >
+                                <span className="name">上海</span>
+                                <i className="iconfont icon-arrow" />
+                            </div>
 
+                            {/* 搜索表单 */}
+                            <div
+                                className="form"
+                                onClick={() => this.props.history.push('/search')}
+                            >
+                                <i className="iconfont icon-seach" />
+                                <span className="text">请输入小区或地址</span>
+                            </div>
+                        </Flex>
+                        {/* 右侧地图图标 */}
+                        <i
+                            className="iconfont icon-map"
+                            onClick={() => this.props.history.push('/map')}
+                        />
+                    </Flex>
+                </div>
+                {/* 轮播图 end*/}
+                
+                {/* item 导航跳转区域 start*/}
                 <Flex className='Nav'>
                     {this.renderNavs()}
                 </Flex>
+                {/* item 导航跳转区域 end*/}
+                
+                {/* 租房小组 start*/}
+                <div className='rentGroup'>
+                    <WingBlank>
+                        <div className='title'>
+                            <h3>租房小组</h3>
+                            <span>更多</span>
+                        </div>
+                        <Grid data={this.state.groups}
+                            square={false}
+                            hasLine={false}
+                            columnNum={2}
+                            renderItem={item => (
+                                <div className='rendGridItem'>
+                                    <Flex>
+                                        <Flex.Item style={{ marginRight: '8px' }}>
+                                            <h4 style={{ marginBottom: '4px' }}>{item.title}</h4>
+                                            <p>{item.desc}</p>
+                                        </Flex.Item>
+                                        <Flex.Item>
+                                            <img src={`http://localhost:8080${item.imgSrc}`}></img>
+                                        </Flex.Item>
+                                    </Flex>
+                                </div>
+                            )}
+                        />
+                    </WingBlank>
 
+                </div>
+                {/* 租房小组 end*/}
+
+                {/* 最新资讯 start */}
+                <div className="news">
+                    <h3 className="group-title">最新资讯</h3>
+                    <WingBlank size="md">{this.renderNews()}</WingBlank>
+                </div>
+                {/* 最新资讯 end */}
 
             </div>
         )
