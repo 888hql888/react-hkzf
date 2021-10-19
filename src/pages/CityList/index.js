f7f1a4701b835f1373ed3680fc0b8df8a7792994
chienli <chienli@chienlideMacBook-Pro.local>
2021-10-19 23:02:55 +0800

    feat:新增城市数据和数据处理

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -7,16 +7,40 @@ export default class CityList extends Component {
     state = {
 
     }
+    // 处理城市数据的方法
+    handleCityList(list = []) {
+        if (list.length == 0) return
+        const CityData = {}
+        list.map(item => {
+            let str = item.short[0]
+            if (CityData[str]) {
+                CityData[str].push(item)
+            } else CityData[str] = []
+        })
+        const CityIndex = Object.keys(CityData).sort()
+        return {
+            CityData,
+            CityIndex
+        }
+    }
     componentDidMount() {
         this.getCityList()
-      }
-    
-      // 获取城市列表数据的方法
-      async getCityList() {
+    }
+
+    // 获取城市列表数据的方法
+    async getCityList() {
         const res = await axios.get('http://localhost:8080/area/city?level=1')
-        console.log('城市列表数据：', res)
-      }
-    
+        // 分离各个字母的城市数据
+        const { CityData, CityIndex } = this.handleCityList(res.data.body)
+        // 添加热门城市
+        const hotRes = await axios.get('http://localhost:8080/area/hot')
+        // console.log('热门城市数据：', hotRes)
+        CityData['hot'] = hotRes.data.body
+        // 将索引添加到 cityIndex 中
+        CityIndex.unshift('hot')
+        
+    }
+
     render() {
         return (
             <div className='cityList'>
